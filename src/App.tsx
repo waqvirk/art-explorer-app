3559acd45554cdd7f88d9acccdf23004d5fcca8e
waqvirk <virkwaqarahmad@gmail.com>
2026-01-23 11:38:35 +0100

    initial project scaffold react+vite (TypeScript) with zod and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-semibold text-white">
+        Art Explorer
+      </h1>
+    </div>
+  )
+}
